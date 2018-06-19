c229edea02a29f8da04271f8d9d0f090b5d4f393
shuz <15558057209@163.com>
2018-06-19 18:22:52 +0800

    react

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
 import './assets/styles/app.scss'
 import logo from './assets/images/logo.svg'
-console.log("hello world");
+import "babel-polyfill";
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+class HelloMessage extends React.Component {
+    render() {
+        return (
+            <div>
+                <div><img src={logo} /></div>
+                <div> Hello {this.props.name}</div>
+            </div>)
+    }
+}
+
+var mountNode = document.getElementById("app");
+ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
