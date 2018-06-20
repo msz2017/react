f1ca0facfc48c0646957bf9b0c02ec005ede170f
shuz <15558057209@163.com>
2018-06-20 11:32:27 +0800

    vue

--- src/app.js
+++ src/app.js
@@ -1,19 +1,9 @@
 import './assets/styles/app.scss'
-import logo from './assets/images/logo.svg'
 import "babel-polyfill";
-import React from "react";
-import ReactDOM from "react-dom";
+import Vue from 'vue'
+import hello from './hello.vue'
 
-
-class HelloMessage extends React.Component {
-    render() {
-        return (
-            <div>
-                <div><img src={logo} /></div>
-                <div> Hello {this.props.name}</div>
-            </div>)
-    }
-}
-
-var mountNode = document.getElementById("app");
-ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
+new Vue({
+    el:'#app',
+    render:h=>h(hello)
+})
---
+++ src/hello.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div>2</div>
+        <h3 class="b">5</h3>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.b{
+    background-color: #f3f3f3;
+}
+</style>
